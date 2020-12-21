77131c293c6e771ce77611413e25b69fa3991e6f
marnyansky <marnyansky@gmail.com>
2020-12-21 21:03:45 +0200

    module 4.2.4

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,5 @@
+from pages.main_page import MainPage
+
 link = "http://selenium1py.pythonanywhere.com/"
 
 
@@ -7,5 +9,6 @@ def go_to_login_page(browser):
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
