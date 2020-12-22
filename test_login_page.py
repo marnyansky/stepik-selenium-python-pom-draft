4a274da190f22dd134d89f76190af88ca0617917
marnyansky <marnyansky@gmail.com>
2020-12-22 07:54:38 +0200

    module 4.2.8: 3 login tests

---
+++ test_login_page.py
@@ -0,0 +1,21 @@
+from pages.login_page import LoginPage
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+
+
+def test_should_be_login_url(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_should_be_login_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_should_be_register_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
