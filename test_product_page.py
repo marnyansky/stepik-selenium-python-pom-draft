c9dbb99b74c70c7197952589f9a3cdc04e99208b
marnyansky <marnyansky@gmail.com>
2020-12-22 21:50:22 +0200

    module 4.3.6 impl

--- test_product_page.py
+++ test_product_page.py
@@ -18,7 +18,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
 ])
-def HIDE_test_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.remember_product_name_and_price_and_add_product_to_basket()
@@ -33,10 +33,12 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.remember_product_name_and_price_and_add_product_to_basket()
     page.should_not_be_success_message()
 
+
 def test_guest_cant_see_success_message(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
+
 
 @pytest.mark.xfail(raises=AssertionError, reason="test should xfail (Stepik.org course > module 4.3.6)")
 def test_message_disappeared_after_adding_product_to_basket(browser):
